b4e185140cad967440e1ed51aaa7ab76bff69e69
murat <killjoy_tr@yahoo.com>
2021-06-10 11:33:04 +0300

    server ts-node ok

--- server/build/index.js
+++ server/build/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
@@ -14,14 +12,12 @@ const indexPath = path_1.default.resolve(__dirname, "../build/index.html");
 app.use("/", express_1.default.static(buildPath));
 app.use("/static", express_1.default.static(staticPath));
 app.all("/", (req, res) => {
-  res.sendFile(indexPath);
+    res.sendFile(indexPath);
 });
 app.post("/api/hello", (req, res) => {
-  const name = req.query.name || "World";
-  res.json({
-    greeting: `Hello ${name}! From2 Expressjs on ${new Date().toLocaleString()}`,
-  });
+    const name = req.query.name || "World";
+    res.json({
+        greeting: `Hello ${name}! From2 Expressjs on ${new Date().toLocaleString()}`,
+    });
 });
-app.listen(3001, () =>
-  console.log("Express server is running on localhost:3001")
-);
+app.listen(3001, () => console.log("Express server is running on localhost:3001"));
